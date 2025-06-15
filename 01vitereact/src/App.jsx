95bffbf1af3a3adecf27ec2c2e9cd129d4553119
Essam Bhatti <essambhatti@gmail.com>
2025-06-15 23:47:37 +0500

    Created my own react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,13 @@
 import Prac from "./prac"
 
 function App() {
-
+  const username = " by Essam Bhatti"
 
   return (
-    // <h1>React+Vite App</h1>
+    <>
+    <h1>React+Vite App {username}</h1>
     <Prac />
+    <p>Test Para</p></>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,7 +3,32 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <h1>This is my own funstion</h1>
+    )
+}
+
+// const reactElement = {
+//     type : 'a',
+//     props : {
+//        href :'https://google.com',
+//        target : '_blank',
+//     },
+//     children : 'Click me to visit google'
+// }
+
+const anotherElement =  (
+    <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+    'a',
+    {'href': 'https://google.com', target : '_blank'},
+    'Click me to visit google'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />,
+    <App/>
 )
