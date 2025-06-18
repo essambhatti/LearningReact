8f6eb547f36150fb54ef843567a364c51bf818d5
Essam Bhatti <essambhatti@gmail.com>
2025-06-18 15:19:27 +0500

    Learned and implemented props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
 import './App.css'
+import Card from './componemts/card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (<h1 className = "text-3xl font-bold ">
+
+  return (
+    <>
+    <h1 className = "bg-green-400 text-white p-4 rounded-xl mb-4" >
       Hello World
-    </h1>)
+    </h1>
+    <Card heading='First card' />
+    <Card heading='Second Card'btntxt='Learn More'/>
+    <Card heading='Third Card' btntxt='Read More'/>
+    
+    </>)
   
 }
 
